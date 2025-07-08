3956eff787a1064c4480072b0d17ad0c0522563d
marube 2005 <emarube89@gmail.com>
2025-07-08 13:59:33 +0300

    Task 3: Updated Task 3b to rank properties by number of bookings using ROW_NUMBER()

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -5,13 +5,13 @@ LEFT JOIN bookings b ON u.id = b.user_id
 GROUP BY u.id, u.username
 ORDER BY booking_count DESC;
 
--- Task 3b: Window function to rank properties based on the total price of their bookings
-SELECT subquery.property_id, subquery.title, subquery.total_booking_price,
-       RANK() OVER (ORDER BY subquery.total_booking_price DESC) AS price_rank
+-- Task 3b: Window function to rank properties based on the total number of bookings they have received
+SELECT subquery.property_id, subquery.title, subquery.booking_count,
+       ROW_NUMBER() OVER (ORDER BY subquery.booking_count DESC) AS booking_rank
 FROM (
-    SELECT p.id AS property_id, p.title, COALESCE(SUM(b.total_price), 0) AS total_booking_price
+    SELECT p.id AS property_id, p.title, COUNT(b.id) AS booking_count
     FROM properties p
     LEFT JOIN bookings b ON p.id = b.property_id
     GROUP BY p.id, p.title
 ) AS subquery
-ORDER BY price_rank;
+ORDER BY booking_rank;
